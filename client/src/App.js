9195a5279762b74adf9b180cb364eee6a4944303
ManuCasanueva <manucasanueva3@gmail.com>
2022-10-24 22:07:12 -0300

    changes  browserRoute

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,23 @@
 import './App.css';
 import LandingPage from './components/LandingPage/LandingPage';
-import { Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Formulario from './components/Formulario/Formulario';
 import RecipeDetail from "./components/RecipesDetails/RecipeDetails"
 
 function App() {
   return (
-    <div className="App">
-
-      <Route exact path="/" component={LandingPage} />
-      {/* <Route path="/home" component={NavBar} /> */}
-      <Route exact path="/home" component={Home} />
-      <Route exact path="/home/newrecipe" component={Formulario} />
-      <Route exact path="/home/recipes/:id" component={RecipeDetail} />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          {/* <Route path="/home" component={NavBar} /> */}
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/home/newrecipe" component={Formulario} />
+          <Route exact path="/home/recipes/:id" component={RecipeDetail} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -10,8 +10,8 @@ import dotenv from "dotenv"
 import axios from "axios"
 dotenv.config();
 
-const { BACK_END_LOCAL } = process.env;
-axios.defaults.baseURL = process.env.BACK_END_DEPLOY || "http://localhost:3001";
+
+axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001/';
 
 ReactDOM.render(
 
